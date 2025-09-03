2e60494210cfd95165275f1f7b357ac50a603b24
Eric Muigai <ericmuigaikims@gmail.com>
2025-09-03 14:51:18 +0300

    moneyz

---
+++ lib/many_to_many.py
@@ -0,0 +1,7 @@
+from .author import Author
+from .magazine import Magazine
+from .article import Article
+from .song import Song
+
+# This file makes importing easier:
+# from lib.many_to_many import Author, Magazine, Article
--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    # class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # update counters
+        Song.count += 1
+
+        # update genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+
+        # update artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+    @classmethod
+    def reset_all(cls):
+        """Reset all class attributes (useful for tests)."""
+        cls.count = 0
+        cls.genres = []
+        cls.artists = []
+        cls.genre_count = {}
+        cls.artist_count = {}
